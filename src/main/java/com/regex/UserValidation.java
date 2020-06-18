0517445c69fa219929d1eff22cd7251cc42f8942
HarithaRugada <haritharugada@gmail.com>
2020-06-18 18:02:47 +0530

    ADDED : Regex For First Name and test cases

--- src/main/java/com/regex/UserValidation.java
+++ src/main/java/com/regex/UserValidation.java
@@ -1,5 +1,14 @@
 package com.regex;
 
-public class UserValidation {
-    
+import java.util.regex.Pattern;
+
+public class UserValidation
+{
+    public static final String FIRST_NAME_REGEX="^[A-Z]{1}[A-Za-z]{2,}$";
+
+    public boolean validateFirstName(String firstName)
+    {
+        Pattern pattern=Pattern.compile(FIRST_NAME_REGEX);
+        return pattern.matcher(firstName).matches();
+    }
 }
--- src/test/java/com/regex/UserRegistrationTest.java
+++ src/test/java/com/regex/UserRegistrationTest.java
@@ -1,6 +1,48 @@
 package com.regex;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserRegistrationTest
 {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue()
+    {
+        UserValidation userValidation=new UserValidation();
+        boolean isValid=userValidation.validateFirstName("Haritha");
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_Length_ShouldReturnFalse()
+    {
+        UserValidation userValidation=new UserValidation();
+        boolean isValid=userValidation.validateFirstName("Ha");
+        Assert.assertFalse(isValid);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ForUpperCase_ShouldReturnFalse()
+    {
+        UserValidation userValidation=new UserValidation();
+        boolean isValid=userValidation.validateFirstName("haritha");
+        Assert.assertFalse(isValid);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ForSpecialCharacter_ShouldReturnFalse()
+    {
+        UserValidation userValidation=new UserValidation();
+        boolean isValid=userValidation.validateFirstName("Hari@tha");
+        Assert.assertFalse(isValid);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ForNumber_ShouldReturnFalse()
+    {
+        UserValidation userValidation=new UserValidation();
+        boolean isValid=userValidation.validateFirstName("Hari123");
+        Assert.assertFalse(isValid);
+    }
 
 }
