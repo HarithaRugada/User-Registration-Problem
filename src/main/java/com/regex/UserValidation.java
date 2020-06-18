3433934e931cd52639deb4d2b8a183f0c319bef1
HarithaRugada <haritharugada@gmail.com>
2020-06-18 20:39:20 +0530

    ADDED : Validation of email

--- src/main/java/com/regex/UserValidation.java
+++ src/main/java/com/regex/UserValidation.java
@@ -1,5 +1,6 @@
 package com.regex;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation
@@ -18,5 +19,22 @@ public class UserValidation
     {
         Pattern pattern=Pattern.compile(LAST_NAME_REGEX);
         return pattern.matcher(lastName).matches();
+    }
+
+    public class ValidateEmail
+    {
+        private Pattern pattern;
+        private Matcher matcher;
+        public static final String EMAIL_REGEX = "^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+
+        public boolean validateEmail()
+        {
+            pattern = Pattern.compile(EMAIL_REGEX);
+        }
+
+        public boolean validate(final String email)
+        {
+            return pattern.matcher(email).matches();
+        }
     }
 }
--- src/test/java/com/regex/UserRegistrationTest.java
+++ src/test/java/com/regex/UserRegistrationTest.java
@@ -1,7 +1,15 @@
 package com.regex;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 public class UserRegistrationTest
 {
@@ -83,6 +91,62 @@ public class UserRegistrationTest
         UserValidation userValidation=new UserValidation();
         boolean isValid=userValidation.validateLastName("Rugada123");
         Assert.assertFalse(isValid);
+    }
+
+    @RunWith(Parameterized.class)
+    public class ValidateEmailTest {
+        private String arg;
+        private static ValidateEmail validateEmail;
+        private Boolean expectedValidation;
+
+        public void ValidateEmailTest(String str, Boolean expectedValidation) {
+            this.arg = str;
+            this.expectedValidation = expectedValidation;
+        }
+
+        @BeforeClass
+        public static void initialize() {
+            validateEmail = new ValidateEmail();
+
+            @Parameterized.Parameters
+            public static Collection<Object[]> data ()
+            {
+                Object[][] data = new Object[][]
+                        {
+                                {"abc@yahoo.com", true},
+                                {"abc-100@yahoo.com", true},
+                                {"abc.100@yahoo.com", true},
+                                {"abc111@abc.com", true},
+                                {"abc-100@abc.net", true},
+                                {"abc.100@abc.com.au", true},
+                                {"abc@1.com", true},
+                                {"abc@gmail.com.com", true},
+                                {"abc+100@gmail.com", true},
+
+                                {"abc", false},
+                                {"abc@.com.my", false},
+                                {"abc123@gmail.a", false},
+                                {"abc123@.com", false},
+                                {"abc123@.com.com", false},
+                                {".abc@abc.com", false},
+                                {"abc()*@gmail.com", false},
+                                {"abc@%*.com", false},
+                                {"abc..2002@gmail.com", false},
+                                {"abc.@gmail.com", false},
+                                {"abc@abc@gmail.com", false},
+                                {"abc@gmail.com.1a", false},
+                                {"abc@gmail.com.aa.au", false}
+                        };
+                return Arrays.asList(data);
+            }
+
+            @Test
+            public void test()
+            {
+                Boolean isValid = userValidation.validateEmail.validate(this.arg);
+                assertEquals("Result", this.expectedValidation,isValid);
+            }
+        }
     }
 
 }
